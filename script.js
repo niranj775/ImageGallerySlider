71677e9327270f0628abaf9dda0189f51b57cf0e
niranj775 <niranj775@gmail.com>
2025-03-13 22:32:40 +0530

    Final App

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("Test");
+let scrollContainter = document.querySelector(".gallery");
+let backBtn = document.getElementById("backBtn");
+let nextBtn = document.getElementById("nextBtn");
+
+scrollContainter.addEventListener("wheel", (evt)=>{
+    evt.preventDefault();
+    scrollContainter.scrollLeft += evt.deltaY;
+    scrollContainter.style.scrollBehavior = "auto";
+});
+
+nextBtn.addEventListener("click", () => {
+    scrollContainter.style.scrollBehavior = "smooth";
+    scrollContainter.scrollLeft += 900;
+});
+
+backBtn.addEventListener("click", () => {
+    scrollContainter.style.scrollBehavior = "smooth";
+    scrollContainter.scrollLeft -= 900;
+});
